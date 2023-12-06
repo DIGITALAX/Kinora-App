08f633d883a375ca98a5dd427b68a50db53e289a
emmajane1313 <admin@digitalax.xyz>
2023-12-05 20:37:39 -0500

    Base

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,43 @@
+import Image from "next/legacy/image";
+import { FunctionComponent } from "react";
+import { INFURA_GATEWAY } from "../../../lib/constants";
+import { NextRouter } from "next/router";
+
+const Footer: FunctionComponent<{ router: NextRouter }> = ({ router }) => {
+  return (
+    <div className="fixed w-full h-20 bottom-0 bg-black flex flex-row items-center justify-center">
+      {[
+        "#0F121A",
+        "#FE0000",
+        "#FE8000",
+        "#FED501",
+        "#FFFFFF",
+        "#0099FF",
+        "#7CDD00",
+      ].map((backgroundColor: string, index: number) => {
+        return (
+          <div
+            key={index}
+            className="relative w-full h-full items-center justify-center flex"
+            style={{ backgroundColor }}
+          >
+            {index == 0 && (
+              <div
+                className="relative w-14 h-8 flex items-center justify-center cursor-pointer active:scale-95"
+                onClick={() => router.push("/")}
+              >
+                <Image
+                  src={`${INFURA_GATEWAY}/ipfs/QmXTBvC2MVytxqjWfLAtyardgapxKzVi8uApTE5aEt1soc`}
+                  layout="fill"
+                  draggable={false}
+                />
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Footer;
