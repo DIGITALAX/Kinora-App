68731a102aebd76d8981fbb8fd113d8714444929
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2023-12-05 20:37:39 -0500

    Base

--- next.config.js
+++ next.config.js
@@ -1,6 +1,53 @@
 /** @type {import('next').NextConfig} */
+
+const allowedOrigins = [
+  "https://api-v2-mumbai.lens.dev/",
+  "https://api-v2.lens.dev/",
+  "https://chromadin.infura-ipfs.io",
+  "https://api.thegraph.com",
+  "https://arweave.net/",
+  "https://gw.ipfs-lens.dev",
+  "https://hey.xyz",
+];
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "chromadin.infura-ipfs.io",
+        pathname: "/ipfs/**",
+      },
+    ],
+    unoptimized: true,
+  },
+  async headers() {
+    let headersConfig = [];
 
-module.exports = nextConfig
+    allowedOrigins.forEach((origin) => {
+      headersConfig.push({
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: origin,
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+        ],
+      });
+    });
+
+    return headersConfig;
+  },
+};
+
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,51 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+  Theme,
+} from "@rainbow-me/rainbowkit";
+import merge from "lodash.merge";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { polygon } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { useRouter } from "next/router";
+import Footer from "@/components/Layout/Footer";
+
+const walletTheme = merge(darkTheme(), {
+  colors: {
+    accentColor: "#111313",
+  },
+} as Theme);
+
+const { chains, publicClient } = configureChains(
+  [polygon],
+  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! })]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Kinora",
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains} theme={walletTheme}>
+        <div className="relative w-full h-full flex flex-col">
+          <Component router={router} {...pageProps} />
+          <Footer router={router} />
+        </div>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
