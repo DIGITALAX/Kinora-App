3bcc02517804bd35255c3c07a1e438e05954c2fb
emmajane1313 <admin@digitalax.xyz>
2024-01-06 17:46:39 -0500

    activity

---
+++ src/components/Activity/modules/Activity.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "react";
+import { ActivityProps } from "../types/activity.types";
+
+const Activity: FunctionComponent<ActivityProps> = (): JSX.Element => {
+    return (
+        <div></div>
+    )
+}
+
+export default Activity;
--- src/pages/activity.tsx
+++ src/pages/activity.tsx
@@ -1,10 +1,54 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { NextRouter } from "next/router";
+import Activity from "@/components/Activity/modules/Activity";
+import useActivity from "@/components/Activity/hooks/useActivity";
+import useInteractions from "@/components/Common/hooks/useInteractions";
+import { useAccount } from "wagmi";
+import { createPublicClient, http } from "viem";
+import { polygonMumbai } from "viem/chains";
+import { Quest } from "@/components/Quest/types/quest.types";
 
 export default function Envoke({ router }: { router: NextRouter }) {
+  const publicClient = createPublicClient({
+    chain: polygonMumbai,
+    transport: http(
+      `https://polygon-mumbai.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY_MUMBAI}`
+    ),
+  });
+  const { address } = useAccount();
+  const dispatch = useDispatch();
   const openSidebar = useSelector(
     (state: RootState) => state.app.sideBarOpenReducer.value
+  );
+  const lensConnected = useSelector(
+    (state: RootState) => state.app.lensConnectedReducer.profile
+  );
+  const {
+    activityLoading,
+    setActivityFeed,
+    getMoreActivityFeed,
+    activityInfo,
+    activityFeed,
+  } = useActivity();
+  const {
+    mirror,
+    like,
+    bookmark,
+    interactionsLoading,
+    setMirrorChoiceOpen,
+    mirrorChoiceOpen,
+    profileHovers,
+    setProfileHovers,
+    followProfile,
+    unfollowProfile,
+  } = useInteractions(
+    lensConnected,
+    dispatch,
+    activityFeed,
+    address,
+    publicClient,
+    (newItems) => setActivityFeed(newItems as Quest[])
   );
 
   return (
@@ -20,7 +64,53 @@ export default function Envoke({ router }: { router: NextRouter }) {
           width: openSidebar ? "calc(100vw - 10rem)" : "calc(100vw - 2.5rem)",
         }}
         id={!openSidebar ? "closeSide" : ""}
-      ></div>
+      >
+        {activityLoading ? (
+          <div className="relative w-full h-fit flex flex-col gap-3">
+            <div className="relative w-full h-fit flex flex-row gap-3">
+              {Array.from({ length: 4 })?.map((_, index: number) => {
+                return (
+                  <div
+                    key={index}
+                    className="relative w-full h-96 flex rounded-sm animate-pulse"
+                    id="rainbow"
+                  ></div>
+                );
+              })}
+            </div>
+            <div className="w-full h-fit grid-cols-2 grid gap-3">
+              {Array.from({ length: 10 }).map((_, index: number) => {
+                return (
+                  <div
+                    key={index}
+                    className="relative w-full h-60 flex rounded-sm animate-pulse"
+                    id="rainbow"
+                  ></div>
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          <Activity
+            activityFeed={activityFeed}
+            getMoreActivityFeed={getMoreActivityFeed}
+            activityInfo={activityInfo}
+            lensConnected={lensConnected}
+            dispatch={dispatch}
+            router={router}
+            mirror={mirror}
+            like={like}
+            interactionsLoading={interactionsLoading}
+            bookmark={bookmark}
+            setMirrorChoiceOpen={setMirrorChoiceOpen}
+            mirrorChoiceOpen={mirrorChoiceOpen}
+            followProfile={followProfile}
+            unfollowProfile={unfollowProfile}
+            setProfileHovers={setProfileHovers}
+            profileHovers={profileHovers}
+          />
+        )}
+      </div>
     </div>
   );
 }
